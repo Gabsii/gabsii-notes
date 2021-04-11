7433ff2607a33bc05be64021c5b3cc451cc79397
Gabsii <lukas.gabsi@gmail.com>
2021-04-12 00:27:42 +0200

    bit more responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,36 @@
-import { Col, Row, Spacer, Text, Toggle, useTheme } from "@geist-ui/react";
+import { Col, Row, Spacer, Text, Toggle, useMediaQuery, useTheme } from "@geist-ui/react";
+import { Moon, Sun } from "@geist-ui/react-icons";
 import { HeaderComponentType } from "../types";
 import Editor from "./Editor";
 
 const Header = ({mdParser, storedNotes, setNotes, onThemeChange}: HeaderComponentType) => {
   const theme = useTheme();
+  const isLG = useMediaQuery('lg');
+
+  console.log(isLG);
 
   return (
     <div style={{ 
       position: 'sticky', 
       backgroundColor: theme.palette.accents_1, 
-      padding: '1rem', 
-      width: '100vw', 
-      transform: 'translateX(-25vw)', 
+      padding: isLG ? '1rem': '1rem 0', 
+      width: isLG ? '100vw' : 'auto', 
+      transform: isLG ? 'translateX(-25vw)' : '', 
       boxShadow: '0 5px 10px rgb(0 0 0 / 12%)' 
     }}>
       <div style={{ 
-        width: '750pt', 
+        maxWidth: '750pt', 
         margin: '0 auto', 
         padding: '0 16pt'
       }}>
-        <Row justify="center">
-          <Text h1>Notes by Gabsii</Text>
-          <Toggle style={{margin: 'auto 0 auto auto'}} onChange={onThemeChange} />
+        <Row justify="center" align="middle">
+          <Col span={22}>
+            <Text h1>Notes by Gabsii</Text>
+          </Col>
+          <Col span={2}>
+            { theme.type === 'light' ? <Sun /> : <Moon /> }
+            <Toggle onChange={onThemeChange} style={{ marginBottom: '10px', marginLeft: '1ch' }} />
+          </Col>
         </Row>
         <Spacer />
         <Row>
